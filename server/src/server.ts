133187c65fb35f3ba9f843b38fe528c2d2b9bcaa
Adrian Mouzinho <adrianmouzinhopro@gmail.com>
2024-04-02 13:23:03 -0300

    chore: add biome setup on server

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,8 +2,6 @@ import fastify from 'fastify'
 
 const app = fastify()
 
-app
-.listen({ port: 3333 })
-.then(() => {
-  console.log('HTTP server running!')
+app.listen({ port: 3333 }).then(() => {
+	console.log('HTTP server running!')
 })
