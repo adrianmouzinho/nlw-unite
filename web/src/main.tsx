2feaada8a08b6d900be955fb83b645918a253be6
Adrian Mouzinho <adrianmouzinhopro@gmail.com>
2024-04-02 09:23:42 -0300

    chore: add tailwind setup

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './app'
+import './index.css'
 
 // biome-ignore lint: document.getElementById('root') should not be null
 ReactDOM.createRoot(document.getElementById('root')!).render(
