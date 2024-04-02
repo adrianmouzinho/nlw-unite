01337c83995cb53f8c1997ae8b0c498d4be3d77f
Adrian Mouzinho <adrianmouzinhopro@gmail.com>
2024-04-02 14:24:51 -0300

    feat: add a route to create an event

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,37 @@
+import { PrismaClient } from '@prisma/client'
 import fastify from 'fastify'
+import z from 'zod'
 
 const app = fastify()
 
-app.get('/', () => {
-	return 'Hello NLW'
+const prisma = new PrismaClient({
+	log: ['query'],
+})
+
+app.post('/events', async (request, reply) => {
+	const createEventSchema = z.object({
+		title: z.string().min(4),
+		details: z.string().nullable(),
+		maximumAttendees: z.number().int().positive().nullable(),
+	})
+
+	const { title, details, maximumAttendees } = createEventSchema.parse(
+		request.body,
+	)
+
+	const event = await prisma.event.create({
+		data: {
+			title,
+			details,
+			slug: new Date().toISOString(),
+			maximumAttendees,
+		},
+		select: {
+			id: true,
+		},
+	})
+
+	return reply.status(201).send({ eventId: event.id })
 })
 
 app.listen({ port: 3333 }).then(() => {
