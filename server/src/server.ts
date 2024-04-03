5048f5f39ab1d093b2013bda84e0dd9d90029973
Adrian Mouzinho <adrianmouzinhopro@gmail.com>
2024-04-03 12:17:55 -0300

    refactor: move create event route to another file

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,38 +1,17 @@
-import { PrismaClient } from '@prisma/client'
 import fastify from 'fastify'
-import z from 'zod'
+import {
+	serializerCompiler,
+	validatorCompiler,
+} from 'fastify-type-provider-zod'
+
+import { createEvent } from './routes/create-event'
 
 const app = fastify()
 
-const prisma = new PrismaClient({
-	log: ['query'],
-})
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
 
-app.post('/events', async (request, reply) => {
-	const createEventSchema = z.object({
-		title: z.string().min(4),
-		details: z.string().nullable(),
-		maximumAttendees: z.number().int().positive().nullable(),
-	})
-
-	const { title, details, maximumAttendees } = createEventSchema.parse(
-		request.body,
-	)
-
-	const event = await prisma.event.create({
-		data: {
-			title,
-			details,
-			slug: new Date().toISOString(),
-			maximumAttendees,
-		},
-		select: {
-			id: true,
-		},
-	})
-
-	return reply.status(201).send({ eventId: event.id })
-})
+app.register(createEvent)
 
 app.listen({ port: 3333 }).then(() => {
 	console.log('HTTP server running!')
