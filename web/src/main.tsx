8a64d0e56443c78cb9cf290410fbc9a5837aebc9
Adrian Mouzinho <adrianmouzinhopro@gmail.com>
2024-04-05 10:34:16 -0300

    chore: add react query and react router dom setup

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,11 +1,17 @@
+import { QueryClientProvider } from '@tanstack/react-query'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { App } from './app'
+import { RouterProvider } from 'react-router-dom'
+
 import './index.css'
+import { queryClient } from './libs/react-query'
+import { router } from './routes'
 
 // biome-ignore lint: document.getElementById('root') should not be null
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
-		<App />
+		<QueryClientProvider client={queryClient}>
+			<RouterProvider router={router} />
+		</QueryClientProvider>
 	</React.StrictMode>,
 )
---
+++ web/src/routes.tsx
@@ -0,0 +1,9 @@
+import { createBrowserRouter } from 'react-router-dom'
+import { App } from './app'
+
+export const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <App />,
+	},
+])
