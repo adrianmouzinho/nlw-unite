50ac8d38e36f61274eacbb8f9c428264da120065
Adrian Mouzinho <adrianmouzinhopro@gmail.com>
2024-04-09 10:43:18 -0300

    refactor: use forward ref on button and label components

--- web/src/components/ui/button.tsx
+++ web/src/components/ui/button.tsx
@@ -1,21 +1,26 @@
-import type { ComponentProps } from 'react'
+import { type ComponentProps, forwardRef } from 'react'
 import { twMerge } from 'tailwind-merge'
 
 interface ButtonProps extends ComponentProps<'button'> {
 	primary?: boolean
 }
 
-export function Button({ primary = false, ...props }: ButtonProps) {
-	return (
-		<button
-			{...props}
-			type={props.type ?? 'button'}
-			className={twMerge(
-				primary
-					? 'h-9 flex items-center gap-2 text-sm font-semibold text-black bg-orange-400 rounded-lg px-3 hover:bg-orange-300 transition-colors'
-					: 'h-9 flex items-center gap-2 text-sm font-semibold text-zinc-50 bg-transparent rounded-lg px-3 border border-white/10 hover:bg-white/5 transition-colors',
-				props.className,
-			)}
-		/>
-	)
-}
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+	({ primary = false, type, className, ...props }, ref) => {
+		return (
+			<button
+				{...props}
+				ref={ref}
+				type={type ?? 'button'}
+				className={twMerge(
+					primary
+						? 'h-9 flex items-center gap-2 text-sm font-semibold text-black bg-orange-400 rounded-lg px-3 hover:bg-orange-300 transition-colors'
+						: 'h-9 flex items-center gap-2 text-sm font-semibold text-zinc-50 bg-transparent rounded-lg px-3 border border-white/10 hover:bg-white/5 transition-colors',
+					className,
+				)}
+			/>
+		)
+	},
+)
+
+export { Button }
--- web/src/components/ui/label.tsx
+++ web/src/components/ui/label.tsx
@@ -1,13 +1,18 @@
-import type { ComponentProps } from 'react'
+import { type ComponentProps, forwardRef } from 'react'
 import { twMerge } from 'tailwind-merge'
 
 interface LabelProps extends ComponentProps<'label'> {}
 
-export function Label(props: LabelProps) {
-	return (
-		<label
-			{...props}
-			className={twMerge('text-sm font-medium block', props.className)}
-		/>
-	)
-}
+const Label = forwardRef<HTMLLabelElement, LabelProps>(
+	({ className, ...props }, ref) => {
+		return (
+			<label
+				{...props}
+				ref={ref}
+				className={twMerge('text-sm font-medium block', className)}
+			/>
+		)
+	},
+)
+
+export { Label }
