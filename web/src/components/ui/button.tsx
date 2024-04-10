1ecc30da2ed3a9f212a517ff1a42453fd3942296
Adrian Mouzinho <adrianmouzinhopro@gmail.com>
2024-04-10 13:36:00 -0300

    styles: reduce opacity when button is disabled

--- web/src/components/ui/button.tsx
+++ web/src/components/ui/button.tsx
@@ -14,8 +14,8 @@ const Button = forwardRef<HTMLButtonElement, ButtonProps>(
 				type={type ?? 'button'}
 				className={twMerge(
 					primary
-						? 'h-9 flex items-center gap-2 text-sm font-semibold text-black bg-orange-400 rounded-lg px-3 hover:bg-orange-300 transition-colors'
-						: 'h-9 flex items-center gap-2 text-sm font-semibold text-zinc-50 bg-transparent rounded-lg px-3 border border-white/10 hover:bg-white/5 transition-colors',
+						? 'h-9 flex items-center gap-2 text-sm font-semibold text-black bg-orange-400 rounded-lg px-3 enabled:hover:bg-orange-300 transition-colors disabled:opacity-50'
+						: 'h-9 flex items-center gap-2 text-sm font-semibold text-zinc-50 bg-transparent rounded-lg px-3 border border-white/10 enabled:hover:bg-white/5 transition-colors disabled:opacity-50',
 					className,
 				)}
 			/>
