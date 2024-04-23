86bc6345719977f69bcf69e9d29e8226675076ba
Adrian Mouzinho <adrianmouzinhopro@gmail.com>
2024-04-23 13:27:33 -0300

    feat: add page to access credentials

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,3 +1,25 @@
+import { Ticket } from 'lucide-react'
+import logoNlwUnite from './assets/logo-nlw-unite.svg'
+import { Button } from './components/button'
+import { Input } from './components/input'
+
 export function App() {
-	return <h1>Hello</h1>
+	return (
+		<div className="w-full h-screen flex items-center justify-center">
+			<form className="max-w-[335px] w-full flex flex-col items-center gap-8">
+				<img src={logoNlwUnite} alt="Logo do NLW Unite" />
+
+				<div className="w-full flex flex-col gap-3">
+					<Input
+						icon={<Ticket className="size-5 text-teal-200" />}
+						aria-label="Acessar credencial"
+						name="code"
+						placeholder="Código do ingresso"
+					/>
+					<Button>Acessar credencial</Button>
+					<Button transparent>Ainda não possui ingresso?</Button>
+				</div>
+			</form>
+		</div>
+	)
 }
