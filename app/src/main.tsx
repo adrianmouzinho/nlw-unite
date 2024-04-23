8ca2b54b407389b83f7cc92e045f6142be7e2357
Adrian Mouzinho <adrianmouzinhopro@gmail.com>
2024-04-23 12:17:47 -0300

    chore: add biome setup

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom/client'
 import { App } from './app'
 import './index.css'
 
+// biome-ignore lint: document.getElementById('root') should not be null
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
 )
